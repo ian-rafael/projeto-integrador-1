8bf8912b7e5d0a29692ee4db227315d790efd042
Ian Rafael <ianrafael2001@gmail.com>
2024-08-01 15:08:11 -0300

    feat: improve dashboard (loan)

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -12,6 +12,11 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   await requireUserId(request);
 
   const today = DateTime.now().startOf("day").toJSDate();
+  const todayPlusOneWeek = DateTime.now().startOf("day").plus({ week: 1 }).toJSDate();
+  const firstDayOfMonth = DateTime.now().startOf("month").toJSDate();
+  const lastDayOfMonth = DateTime.now().endOf("month").toJSDate();
+
+  // VENDA
   const lateInstallmentsPromise = db.saleInstallment.findMany({
     select: {
       id: true,
@@ -21,9 +26,9 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
       sale: { select: { customer: { select: { name: true } } } },
     },
     where: { status: $Enums.StatusParcela.PENDENTE, dueDate: { lt: today } },
+    orderBy: { dueDate: 'asc' },
   });
 
-  const todayPlusOneWeek = DateTime.now().startOf("day").plus({ week: 1 }).toJSDate();
   const nextInstallmentsPromise = db.saleInstallment.findMany({
     select: {
       id: true,
@@ -33,15 +38,15 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
       sale: { select: { customer: { select: { name: true } } } },
     },
     where: { status: $Enums.StatusParcela.PENDENTE, dueDate: { gte: today, lt: todayPlusOneWeek } },
+    orderBy: { dueDate: 'asc' },
   });
 
-  const firstDayOfMonth = DateTime.now().startOf("month").toJSDate();
-  const lastDayOfMonth = DateTime.now().endOf("month").toJSDate();
   const inflowPromise = db.saleInstallment.aggregate({
     _sum: { value: true },
     where: { paymentDate: { lte: lastDayOfMonth, gte: firstDayOfMonth } },
   });
 
+  // COMPRA
   const toBeReceivedProductsPromise = db.productPurchase.findMany({
     select: {
       productId: true,
@@ -61,18 +66,50 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
     where: { purchase: { createdAt: { lte: lastDayOfMonth, gte: firstDayOfMonth } } },
   });
 
+  // EMPRESTIMO
+  const lentProductsPromise = db.productLoan.findMany({
+    select: { quantity: true, unitPrice: true },
+    where: { loan: { createdAt: { lte: lastDayOfMonth, gte: firstDayOfMonth } } },
+  });
+
+  const lateReturnsPromise = db.loan.findMany({
+    select: { id: true, dueDate: true, customer: { select: { name: true } } },
+    where: {
+      sale: null,
+      dueDate: { lt: today },
+      productItems: { some: { returnedQuantity: { lt: db.productLoan.fields.quantity } } },
+    },
+    orderBy: { dueDate: 'asc' },
+  });
+
+  const nextReturnsPromise = db.loan.findMany({
+    select: { id: true, dueDate: true, customer: { select: { name: true } } },
+    where: {
+      sale: null,
+      dueDate: { gte: today, lt: todayPlusOneWeek },
+      productItems: { some: { returnedQuantity: { lt: db.productLoan.fields.quantity } } },
+    },
+    orderBy: { dueDate: 'asc' },
+  });
+
   const [
     lateInstallments,
     nextInstallments,
     inflow,
     toBeReceivedProducts,
     boughtProducts,
+    lentProducts,
+    lateReturns,
+    nextReturns,
   ] = await Promise.all([
     lateInstallmentsPromise,
     nextInstallmentsPromise,
     inflowPromise,
     toBeReceivedProductsPromise,
     boughtProductsPromise,
+    lentProductsPromise,
+    lateReturnsPromise,
+    nextReturnsPromise,
   ]);
 
   return json({
@@ -81,6 +118,9 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
     nextInstallments,
     toBeReceivedProducts,
     outflow: boughtProducts.reduce((acc, data) => acc + data.quantity * data.unitPrice, 0),
+    loanInflow: lentProducts.reduce((acc, data) => acc + data.quantity * data.unitPrice, 0),
+    lateReturns,
+    nextReturns,
   });
 };
 
@@ -91,6 +131,9 @@ export default function App () {
     inflow,
     toBeReceivedProducts,
     outflow,
+    loanInflow,
+    lateReturns,
+    nextReturns,
   } = useLoaderData<typeof loader>();
 
   return (
@@ -165,6 +208,46 @@ export default function App () {
                   {` (${data.receivedQuantity}/${data.quantity})`}
                   {` - `}
                   {data.purchase.supplier.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <h3 className="bg-slate-500 text-slate-50 px-2 py-1">Empréstimo</h3>
+          <p>
+            Recebido no mês:
+            {' '}
+            <span className="text-green-600">
+              {loanInflow && formatCurrency(loanInflow)}
+            </span>
+          </p>
+          <h4>Retornos atrasados:</h4>
+          <ul>
+            {lateReturns.map((data) => (
+              <li key={data.id}>
+                <Link
+                  className="text-blue-600 underline"
+                  to={`emprestimos/${data.id}`}
+                >
+                  {formatDate(data.dueDate)}
+                  {' - '}
+                  {data.customer.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <h4>Próximos retornos:</h4>
+          <ul>
+            {nextReturns.map((data) => (
+              <li key={data.id}>
+                <Link
+                  className="text-blue-600 underline"
+                  to={`emprestimos/${data.id}`}
+                >
+                  {formatDate(data.dueDate)}
+                  {' - '}
+                  {data.customer.name}
                 </Link>
               </li>
             ))}
